cf2d7855a82bcedcd0e26e1f800d15e21099f35f
Ansul Joshi <joshideepak920@gmail.com>
2026-05-14 13:50:23 +0530

    delete all the files form the localreop

--- 07_zustand/src/store/Countstore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand"
-export const CountStore = create((set) => (
-    {
-        Count: 0,
-        increaseCount: () => set((state) => ({ Count: state.Count + 1 })),
-        decreaseCount: () => set((state) => ({ Count: state.Count - 1 })),
-        reset: () => set({ Count: 0 })
-    }
-))
